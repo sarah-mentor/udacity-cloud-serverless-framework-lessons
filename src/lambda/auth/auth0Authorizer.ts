00283cdfe8685be6317fb267557413d7d08a49e5
Sarah Maris <sarah@sarahmaris.com>
2019-08-07 18:38:16 -0400

    Add  AWS Secrets Manager - lesson 5.6

--- src/lambda/auth/auth0Authorizer.ts
+++ src/lambda/auth/auth0Authorizer.ts
@@ -4,17 +4,25 @@ import {
   CustomAuthorizerHandler
 } from 'aws-lambda';
 import 'source-map-support/register';
+import * as AWS from 'aws-sdk'
+
 
 import { verify } from 'jsonwebtoken';
 import { JwtToken } from '../../auth/JwtToken';
 
-const auth0Secret = process.env.AUTH_0_SECRET;
+const secretID = process.env.AUTH_0_SECRET_ID;
+const secretField = process.env.AUTH_0_SECRET_FIELD;
+
+const client = new AWS.SecretsManager();
+
+// Cache secret if a Lambda instance is reused
+let cachedSecret: string
 
 export const handler: CustomAuthorizerHandler = async (
   event: CustomAuthorizerEvent
 ): Promise<CustomAuthorizerResult> => {
   try {
-    const decodedToken = verifyToken(event.authorizationToken);
+    const decodedToken = await verifyToken(event.authorizationToken);
     console.log('User was authorized');
 
     return {
@@ -49,7 +57,7 @@ export const handler: CustomAuthorizerHandler = async (
   }
 };
 
-function verifyToken(authHeader: string): JwtToken {
+async function verifyToken(authHeader: string): Promise<JwtToken> {
   if (!authHeader) throw new Error('No authentication header');
 
   if (!authHeader.toLowerCase().startsWith('bearer '))
@@ -58,7 +66,23 @@ function verifyToken(authHeader: string): JwtToken {
   const split = authHeader.split(' ');
   const token = split[1];
 
-  return verify(token, auth0Secret) as JwtToken;
+  const secretObject: any = await getSecret()
+  const secret = secretObject[secretField]
 
   // A request has been authorized.
+  return verify(token, secret) as JwtToken;
+
+}
+
+async function getSecret() {
+  if (cachedSecret) return cachedSecret;
+
+  const data = await client
+    .getSecretValue({
+      SecretId: secretID
+    }).promise();
+
+  cachedSecret = data.SecretString;
+
+  return JSON.parse(cachedSecret);
 }
