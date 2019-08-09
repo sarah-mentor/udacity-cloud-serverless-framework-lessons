50ecdf06e9316a66c697718cbe55d0bdaabf23ef
Sarah Maris <sarah@sarahmaris.com>
2019-08-08 22:45:45 -0400

    Restructure using ports and adapters - lesson 6.2

--- src/lambda/http/getGroups.ts
+++ src/lambda/http/getGroups.ts
@@ -1,27 +1,11 @@
-import {
-  APIGatewayProxyHandler,
-  APIGatewayProxyEvent,
-  APIGatewayProxyResult
-} from 'aws-lambda';
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import 'source-map-support/register';
-import * as AWS from 'aws-sdk';
+import { getAllGroups } from '../../businessLogic/groups';
 
-const docClient = new AWS.DynamoDB.DocumentClient();
-
-const groupsTable = process.env.GROUPS_TABLE;
-
-export const handler: APIGatewayProxyHandler = async (
-  event: APIGatewayProxyEvent
-): Promise<APIGatewayProxyResult> => {
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event);
 
-  const result = await docClient
-    .scan({
-      TableName: groupsTable
-    })
-    .promise();
-
-  const items = result.Items;
+  const groups = await getAllGroups();
 
   return {
     statusCode: 200,
@@ -29,7 +13,7 @@ export const handler: APIGatewayProxyHandler = async (
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
-      items
+      items: groups
     })
   };
-};
+}
